640f6c5379e4b1a4be462f946ad9d0b710beaf59
sabinmaharjan <sabinmaharjan0@gmail.com>
2026-08-31 19:49:50 +0545

    error handling

--- gussing-game.py
+++ gussing-game.py
@@ -3,10 +3,13 @@ random_number = random.randint(1,100)
 number = 0
 
 while number != random_number:
-    number = int(input("enter a number"))
-    if number > random_number:
-        print("number is less")
-    elif number < random_number:
-        print("number is greater")
-    else:
-        print("you found the number")
+    try:
+        number = int(input("enter a number"))
+        if number > random_number:
+            print("number is less")
+        elif number < random_number:
+            print("number is greater")
+        else:
+            print("you found the number")
+    except ValueError:
+        print("please type a number")
