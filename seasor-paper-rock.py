919e848c3a9b73b92e2bac2b3cf9e96cd7aa7bf8
sabinmaharjan <sabinmaharjan0@gmail.com>
2026-08-31 20:12:49 +0545

    impoverd loop

--- seasor-paper-rock.py
+++ seasor-paper-rock.py
@@ -8,19 +8,20 @@ user = ""
 while user not in ["s","p","r"]:
     user = str(input("input s for scissor, p for paper and r for rock: "))
 
-    print("computer choosed : ", computermove)
 
-    if (user == computermove):
-        print("draw")
-    elif(user == "s" and computermove == "p" ):
-        print("you win")
-    elif(user == "p" and computermove == "s"):
-        print("you loose")
-    elif(user=="r" and computermove == "s"):
-        print("you win")
-    elif(user=="s" and computermove == "r"):
-        print("you loose")
-    elif(user=="p" and computermove == "r"):
-        print("you win")
-    elif(user=="r" and computermove == "p"):
-        print("you loose")
+print("computer choosed : ", computermove)
+
+if (user == computermove):
+    print("draw")
+elif(user == "s" and computermove == "p" ):
+    print("you win")
+elif(user == "p" and computermove == "s"):
+    print("you loose")
+elif(user=="r" and computermove == "s"):
+    print("you win")
+elif(user=="s" and computermove == "r"):
+    print("you loose")
+elif(user=="p" and computermove == "r"):
+    print("you win")
+elif(user=="r" and computermove == "p"):
+    print("you loose")
