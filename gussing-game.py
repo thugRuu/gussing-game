499ed737adf17adbee950d944f4e49c397ac2ba8
sabinmaharjan <sabinmaharjan0@gmail.com>
2026-08-31 19:47:42 +0545

    random number

---
+++ gussing-game.py
@@ -0,0 +1,12 @@
+import random
+random_number = random.randint(1,100)
+number = 0
+
+while number != random_number:
+    number = int(input("enter a number"))
+    if number > random_number:
+        print("number is less")
+    elif number < random_number:
+        print("number is greater")
+    else:
+        print("you found the number")
