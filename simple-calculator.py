4e73004385e452c04afbbba647ae5e2f53d53020
sabinmaharjan <sabinmaharjan0@gmail.com>
2026-08-31 20:30:50 +0545

    correction calculator

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,6 +1,6 @@
 number1 = None
 number2 = None
-while number1 == None and number2 == None:
+while number1 == None or number2 == None:
     try:
         number1 = int(input("1st number"))
         number2 = int(input("2st number"))
@@ -11,7 +11,7 @@ operators = ["+", "-", "*","/"]
 selectoperator = ""
 while selectoperator not in operators:
     selectoperator = str(input("enter operator + for addition, - for substration, * for multipication and / for division"))
-    
+
 match selectoperator:
     case "+":
         print(number1+number2)
