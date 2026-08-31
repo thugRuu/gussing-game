0ccbab7280c7664731f7d41ac280ce1eaa00ec70
sabinmaharjan <sabinmaharjan0@gmail.com>
2026-08-31 20:27:43 +0545

    simple calculator

---
+++ simple-calculator.py
@@ -0,0 +1,23 @@
+number1 = None
+number2 = None
+while number1 == None and number2 == None:
+    try:
+        number1 = int(input("1st number"))
+        number2 = int(input("2st number"))
+    except ValueError:
+        print("enter a number")
+
+operators = ["+", "-", "*","/"]
+selectoperator = ""
+while selectoperator not in operators:
+    selectoperator = str(input("enter operator + for addition, - for substration, * for multipication and / for division"))
+    
+match selectoperator:
+    case "+":
+        print(number1+number2)
+    case "-":
+        print(number1-number2)
+    case "*":
+        print(number1*number2)
+    case "/":
+        print(number1/number2)
